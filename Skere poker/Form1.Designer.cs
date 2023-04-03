87670251492ae6a39bed359ec2fb550c032d6eea
kaajjaak <yolobroer202@gmail.com>
2023-04-03 13:55:25 +0200

    Add project files.

---
+++ Skere poker/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Skere_poker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(688, 187);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(528, 187);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(372, 187);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(208, 187);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(49, 187);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(688, 38);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(528, 38);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(372, 38);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(208, 38);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(49, 38);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(829, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "opponent";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(829, 209);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "joe mama";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 373);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Dice Poker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Skere poker/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skere_poker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //om random getallen te genereren
+        Random random = new Random();
+        //lijst met alle afbeeldingen
+        List<Bitmap> images = new List<Bitmap> { Properties.Resources.dobbel1, Properties.Resources.dobbel2, Properties.Resources.dobbel3, Properties.Resources.dobbel4, Properties.Resources.dobbel5, Properties.Resources.dobbel6 };
+        //1 lijst per hand
+        List<int> nummers1 = new List<int>();
+        List<int> nummers2 = new List<int>();
+        private void controleer()
+        {
+            //vraagt de scores op en update de labels
+            Tuple<int, string, int> score1 = Score(nummers1);
+            Tuple<int, string, int> score2 = Score(nummers2);
+            label2.Text = $"You had a {score1.Item2}\nYour score is: {score1.Item1}";
+            label1.Text = $"Your opponent had a {score2.Item2}\nTheir score is: {score2.Item1}";
+            int tocheck1, tocheck2;
+            //controlleert of gelijk spel is, indien dit zo is zal het ervoor zorgen dat het de gelijkspel score controleerd in de plaats van de totaalscore
+            if (score1.Item1 == score2.Item1)
+            {
+                label2.Text += $"\nTie score: {score1.Item3}";
+                label1.Text += $"\nTie score: {score2.Item3}";
+                tocheck1 = score1.Item3;
+                tocheck2 = score2.Item3;
+            }
+            else
+            {
+                tocheck1 = score1.Item1;
+                tocheck2 = score2.Item1;
+            } //contoleert wie winnaar is door de te checken scores te vergelijken en toont messagebox met of je gewonnen bent of niet
+            if (tocheck1 > tocheck2)
+            {
+                MessageBox.Show("You won!", "EZ");
+            }
+            else if (tocheck1 < tocheck2)
+            {
+                MessageBox.Show("You lost", "NOOB");
+            }
+            else
+            {
+                MessageBox.Show("You Tied", "Better luck next time!");
+            }
+        }
+        private Tuple<int, string, int> Score(List<int> nummer1)
+        {
+            //roept alle functies die ik gemaakt heb om te zoeken welke score je hebt op, de eerste returnwaarde is de score, de tweede wat je precies hebt en de derde is de score die je wilt controleren indien er gelijkspel is
+            if (straightflush(nummer1))
+            {
+                return Tuple.Create(7, "Straight Flush", nummer1.Sum());
+            }
+            else if (fiveofakind(nummer1).Item1)
+            {
+                return Tuple.Create(6, "Five of a kind", fiveofakind(nummer1).Item2);
+            }
+            else if (fourofakind(nummer1).Item1)
+            {
+                return Tuple.Create(5, "Four of a kind", fourofakind(nummer1).Item2);
+            }
+            else if (fullhouse(nummer1))
+            {
+                return Tuple.Create(4, "Fullhouse", nummer1.Sum());
+            }
+            else if (threeofakind(nummer1).Item1)
+            {
+                return Tuple.Create(3, "Three of a kind", threeofakind(nummer1).Item2);
+            }
+            else if (twopair(nummer1).Item1)
+            {
+                return Tuple.Create(2, "Two Pairs", twopair(nummer1).Item2 + twopair(nummer1).Item3);
+            }
+            else if (pair(nummer1).Item1)
+            {
+                return Tuple.Create(1, "Pair", pair(nummer1).Item2);
+            }
+            return Tuple.Create(0, "You Suck!", 0);
+
+        }
+        private bool straightflush(List<int> nummer1)
+        {  //sorteert eerst de lijst en gaat dan na of elke variabele in de lijst vanaf de tweede gelijk is aan de vorige +1
+            nummer1.Sort();
+            for (int i = 1; i < nummer1.Count(); i++)
+            {
+                if (nummer1[i] != nummer1[i - 1] + 1)
+                    return false;
+            }
+            return true;
+        }
+        private Tuple<int, List<int>> ofakind(int amount, List<int> nummer1)
+        { //kijkt voor elke waarde van 1 tot 6 of het getal (van 1 tot 6) "amount" keer in de lijst zit, het returned het aantal keer dit het geval is met daarnaast een lijst van alle getallen waarvoor dit het geval is
+            int totalamm = 0;
+            List<int> kinds = new List<int>();
+            for (int i = 1; i <= 6; i++)
+            {
+                if (nummer1.Count(getal => getal == i) == amount)
+                {
+                    totalamm++;
+                    kinds.Add(i);
+                }
+            }
+            return Tuple.Create(totalamm, kinds);
+        }
+        private Tuple<bool, int> fourofakind(List<int> nummer1)
+        { //roept de functie ofakind() op en als deze functie returned dat er 1 keer 4 dezelfde getallen zijn dan returned het true met het getal dat 4 keer voorkomt (het is geen lijst meer omdat er maar 1 mogelijk is in dit geval)
+            Tuple<int, List<int>> tocheck = ofakind(4, nummer1);
+            if (tocheck.Item1 == 1)
+                return Tuple.Create(true, tocheck.Item2[0]);
+            return Tuple.Create(false, 0);
+        }
+        private Tuple<bool, int> fiveofakind(List<int> nummer1)
+        {//hetzelfde als fourofakind() maar dan met 5
+            Tuple<int, List<int>> tocheck = ofakind(5, nummer1);
+            if (tocheck.Item1 == 1)
+                return Tuple.Create(true, tocheck.Item2[0]);
+            return Tuple.Create(false, 0);
+        }//roept de functie three of a kind en pair op en returned true als beide waar zijn
+        private bool fullhouse(List<int> nummer1)
+        {
+            if (threeofakind(nummer1).Item1 && pair(nummer1).Item1)
+                return true;
+            return false;
+        }
+        private Tuple<bool, int> threeofakind(List<int> nummer1)
+        {//zelfde als fourofakind en fiveofakind
+            Tuple<int, List<int>> tocheck = ofakind(3, nummer1);
+            if (tocheck.Item1 == 1)
+                return Tuple.Create(true, tocheck.Item2[0]);
+            return Tuple.Create(false, 0);
+        }
+        private Tuple<bool, int, int> twopair(List<int> nummer1)
+        {//controlleerd met ofakind hoeveel keer 2 dezelfde getallen voorkomen en indien dit 2 keer is dan returned het true met de waarden van de 2 unieke dobbelstenen uit de paren
+            Tuple<int, List<int>> tocheck = ofakind(2, nummer1);
+            if (ofakind(2, nummer1).Item1 == 2)
+                return Tuple.Create(true, tocheck.Item2[0], tocheck.Item2[1]);
+            return Tuple.Create(false, 0, 0);
+        }
+        private Tuple<bool, int> pair(List<int> nummer1)
+        { //zelfde als fourofakind threeofakind en fiveofakind
+            Tuple<int, List<int>> tocheck = ofakind(2, nummer1);
+            if (tocheck.Item1 == 1)
+                return Tuple.Create(true, tocheck.Item2[0]);
+            return Tuple.Create(false, 0);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //cleared de twee handen zodat je niet met de handen van de vorige game speelt
+            nummers1.Clear();
+            nummers2.Clear();
+            //itereert over alle pictureboxen in de form
+            foreach (PictureBox pb in this.Controls.OfType<PictureBox>())
+            {
+                //maakt random getal tussen 1 en 6
+                int randomgetal = random.Next(1, 7);
+                //stelt de pictureboxen in met de juist image, de -1 is omdat images[] een lijst is en lijsten beginnen te tellen vanaf 0 en aangezien er 6 afbeeldingen in de lijst zitten is dobbelsteen 1, de 0de foto in de lijst
+                pb.Image = images[randomgetal - 1];
+                //als de eerste hand als 5 dobbelstenen heeft dan zal hij aan de tweede hand toevoegen
+                if (nummers1.Count == 5)
+                {
+                    nummers2.Add(randomgetal);
+                }
+                else
+                {
+                    nummers1.Add(randomgetal);
+                }
+            }
+            controleer();
+        }
+    }
+}
